8ce7070a31fbc388fcf4dfb0b849d1204905a98b
Tony <tony.saintamour@gmail.com>
2024-04-22 11:57:36 -0400

    I added comments to script.js for better understanding

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// represents a reference to an HTML element
 const playlistSongs = document.getElementById("playlist-songs");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
@@ -5,6 +6,8 @@ const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
+
+// An array of objects that contains all the songs I have and their information
 const allSongs = [
   {
     id: 0,
@@ -78,63 +81,115 @@ const allSongs = [
   },
 ];
 
+// new is a keyword used to create a new instance of an object
+// the Audio() object represents an audio element in the HTML docuemnt
+// Used to play sounds or music on a web page
 const audio = new Audio();
+
+// an object used to track the songs a user is playing 
 let userData = {
+  // the spread '...' operator is used to create a new copy of allSongs array
+  // Giving userData its own independent copy of the songs
   songs: [...allSongs],
   currentSong: null,
   songCurrentTime: 0,
 };
 
+// responsible for playing a specific song from the playlist
+// in the paramater the id is the unique identifier of the song to be played
 const playSong = (id) => {
+
+  // ? symbol is part of the optional chaining operator
+  // if the value before the ? is NOT null or undefined, it proceeds 
+  // if the is the expresion short circuits and evaluates to undefined without attempting to access or call
+  // used if you are not sure if an object or its properties exist
+  // this line attempts to find the song with the provided id
   const song = userData?.songs.find((song) => song.id === id);
+
+  // sets src of the song being played to audio element
   audio.src = song.src;
+
+  // sets title of the song being played to audio element
   audio.title = song.title;
 
+  // checks if there is a currently No song playing, optional chaining is used to avoid error
+  // or if the currently playing song's id is different from the id of song about to be played
   if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    // starts playback from beginning
     audio.currentTime = 0;
   } else {
+    // resumes playback of current song to where it was paused
     audio.currentTime = userData?.songCurrentTime;
   }
+
+  // saves song to the users current song being played
   userData.currentSong = song;
+
+  // provides access to the list of CSS classes of element
+  // add() is a method that adds the specified class
+  // details for class found in styles.css
   playButton.classList.add("playing");
 
+  // calls the respected functions defined below
   highlightCurrentSong();
   setPlayerDisplay();
   setPlayButtonAccessibleText();
+
+  // calls built in method in JS 
+  // starts playing audio from its current position
   audio.play();
 };
 
 const pauseSong = () => {
+  // currentTime is a property of the HTMLMediaElement interface, which audio represents.
+  // userData.songCurrentTime ensures when user pauses, the playback position is saved in the userData object.
   userData.songCurrentTime = audio.currentTime;
   
+  // removes the "playing" classList from playbutton html
   playButton.classList.remove("playing");
+
+  //pauses the audio
   audio.pause();
 };
 
 const playNextSong = () => {
+  // Check if no current song is playing
   if (userData?.currentSong === null) {
+    // If no current song, play first song in playlist
     playSong(userData?.songs[0].id);
   } else {
+    // if there is a current song
+    // Get index of current song in playlist
     const currentSongIndex = getCurrentSongIndex();
+    // get next song in playlist
     const nextSong = userData?.songs[currentSongIndex + 1];
 
+    //play the next song in playlist
     playSong(nextSong.id);
   }
 };
 
 const playPreviousSong = () =>{
-   if (userData?.currentSong === null) return;
+    // if not current song is playing
+   if (userData?.currentSong === null) return; // return exist function preventing rest of code from executing
    else {
+    // if there is a current song
+    // Get index of current song in playlist
     const currentSongIndex = getCurrentSongIndex();
+    // get previous sonf in playlist
     const previousSong = userData?.songs[currentSongIndex - 1];
 
+    // play the previous song in playlist
     playSong(previousSong.id);
    }
 };
 
 const shuffle = () => {
+  // sorts songs in userData array using sort, math, and random methods 
   userData?.songs.sort(() => Math.random() - 0.5);
+  // assign current song to no song
   userData.currentSong = null;
+  // sets songs current time to zero
   userData.songCurrentTime = 0;
 
   renderSongs(userData?.songs);
@@ -144,33 +199,50 @@ const shuffle = () => {
 };
 
 const deleteSong = (id) => {
+
+  // checking if current song playing matches song user chooses to delete
   if (userData?.currentSong?.id === id) {
+    // sets current song to no song
     userData.currentSong = null;
+    // restarts current time back to zero
     userData.songCurrentTime = 0;
 
     pauseSong();
     setPlayerDisplay();
   }
-
+  
+  // The filter method creates a new array by filtering out the song with the provided id from the userData.songs array.
+  // Each song is checked, and the filter returns true only for songs whose id does not match the provided id.
   userData.songs = userData?.songs.filter((song) => song.id !== id);
+
   renderSongs(userData?.songs); 
   highlightCurrentSong(); 
   setPlayButtonAccessibleText(); 
 
+  // checks if there is no song in songs array
   if (userData?.songs.length === 0) {
+    // creates the reset button in HTML document
     const resetButton = document.createElement("button");
+    // creates text in HTML document
     const resetText = document.createTextNode("Reset Playlist");
-
+    // gives resetButton the reset id
     resetButton.id = "reset";
+    // give reset button a aria-label
     resetButton.ariaLabel = "Reset playlist";
+
+    // modifies DOM structure of resetButton and playlistSongs by adding new element to node
     resetButton.appendChild(resetText);
     playlistSongs.appendChild(resetButton);
 
+    // listens for clicks on resestButton
     resetButton.addEventListener("click", () => {
+      // spreads and assigns all songs in allSongs array to songs in userData object
       userData.songs = [...allSongs];
 
       renderSongs(sortSongs()); 
       setPlayButtonAccessibleText();
+
+      // Removes the reset button from the DOM after the playlist is reset and its functionality is no longer needed.
       resetButton.remove();
     });
 
@@ -179,29 +251,43 @@ const deleteSong = (id) => {
 };
 
 const setPlayerDisplay = () => {
+  // assigns id elements to a const
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
+
+  // assigns song Title and Artist to a const
   const currentTitle = userData?.currentSong?.title;
   const currentArtist = userData?.currentSong?.artist;
 
+  // textContent is a built-in property in JS
+  // updating text content of HTML represent song Title and Artist
   playingSong.textContent = currentTitle ? currentTitle : "";
   songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
+  // assigns HTML id to a constant
+  // the id isn't explicitly found in html document, it is rendered within renderSongs() 
   const playlistSongElements = document.querySelectorAll(".playlist-song");
+
+  // highlights based on id of current playing song
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
 
+  // Loop through each song element in the playlist and remove the 'aria-current' attribute,
+  // which highlights the currently playing song in the playlist.
   playlistSongElements.forEach((songEl) => {
     songEl.removeAttribute("aria-current");
   });
 
+  // If there's a song to highlight, set its 'aria-current' attribute to 'true',
+  // indicating that it's the currently playing song in the playlist.
   if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
 const renderSongs = (array) => {
+  // Map through each song in the array and create html for it
   const songsHTML = array
     .map((song)=> {
       return `
@@ -218,26 +304,36 @@ const renderSongs = (array) => {
       </li>
       `;
     })
-    .join("");
+    .join(""); // Jointhe HTML string together in one string
 
+  // Set the innerHtml of playlistSongs element to the generated html
   playlistSongs.innerHTML = songsHTML;
 };
 
 const setPlayButtonAccessibleText = () => {
+  // Get the current song from userData, or the first song if no current song is available
   const song = userData?.currentSong || userData?.songs[0];
 
+  // Set the aria-label attribute of the playButton
   playButton.setAttribute(
+    // If the song title exists, set the aria-label to "Play [song title]", otherwise set it to "Play"
     "aria-label",
     song?.title ? `Play ${song.title}` : "Play"
   );
 };
 
-const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
+const getCurrentSongIndex = () => {
+  // Get the index of the current song in the userData.songs array
+  // it is important to track the position of current playin song to perform various actions
+  return userData?.songs.indexOf(userData?.currentSong);
+}
 
 playButton.addEventListener("click", () => {
-    if (userData?.currentSong === null) {
+  // if there is no current song, play the first song in playlist
+  if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
   } else {
+    // else play the song selected
     playSong(userData?.currentSong.id);
   }
 });
@@ -250,24 +346,29 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
-
+// handles the "eneded" event of audio element
 audio.addEventListener("ended", () => {
+  // get index of current playing song
     const currentSongIndex = getCurrentSongIndex();
+    // checks if next song exist in the array
     const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
   
-      if (nextSongExists) {
-        playNextSong();
-      } else {
-        userData.currentSong = null;
-        userData.songCurrentTime = 0;
-        pauseSong()
-setPlayerDisplay()
-highlightCurrentSong()
-setPlayButtonAccessibleText()
-}
+    // if there is a next song play it
+    if (nextSongExists) {
+      playNextSong();
+    } else {
+      // if there is not next song, reset the current song and player state
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;
+      pauseSong()
+      setPlayerDisplay()
+      highlightCurrentSong()
+      setPlayButtonAccessibleText()
+    }
 });
 
 const sortSongs = () => {
+  // sort the songs array alphabetically by their titles
     userData?.songs.sort((a,b) => {
       if (a.title < b.title) {
         return -1;
@@ -280,8 +381,9 @@ const sortSongs = () => {
       return 0;
     });
   
+    // return the sorted array
     return userData?.songs;
-  };
+};
   
-  renderSongs(sortSongs());
-  setPlayButtonAccessibleText();
+renderSongs(sortSongs());
+setPlayButtonAccessibleText();
